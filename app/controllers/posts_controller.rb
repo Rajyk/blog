8216f19eba4c5673cfb32a0f8c9f09680a5a76c7
Raj Yelamanchili <rajy@squareup.com>
2018-02-14 16:30:20 -0800

    updated posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,28 +26,27 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(post_params)
 
-    respond_to do |format|
+/    respond_to do |format| /
       if @post.save
-        format.html { redirect_to @post, notice: 'Post was successfully created.' }
-        format.json { render :show, status: :created, location: @post }
+        redirect_to @post, notice: 'Post was successfully created.'
       else
-        format.html { render :new }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
+        render :new
+/        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end /
     end
   end
 
   # PATCH/PUT /posts/1
   # PATCH/PUT /posts/1.json
   def update
-    respond_to do |format|
+ /   respond_to do |format|/
       if @post.update(post_params)
-        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
-        format.json { render :show, status: :ok, location: @post }
+        redirect_to @post, notice: 'Post was successfully updated.'
+  /      format.json { render :show, status: :ok, location: @post } /
       else
-        format.html { render :edit }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
+        render :edit
+  /      format.json { render json: @post.errors, status: :unprocessable_entity }/
+
     end
   end
 
@@ -55,10 +54,10 @@ class PostsController < ApplicationController
   # DELETE /posts/1.json
   def destroy
     @post.destroy
-    respond_to do |format|
-      format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+/    respond_to do |format| /
+      redirect_to posts_url, notice: 'Post was successfully destroyed.'
+  /    format.json { head :no_content }
+    end/
   end
 
   private
@@ -69,6 +68,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:name, :title, :content)
+      params.require(:post).permit(:title, :body)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180110203329) do
+ActiveRecord::Schema.define(version: 20180207202925) do
 
   create_table "posts", force: :cascade do |t|
-    t.string "name"
     t.string "title"
-    t.text "content"
+    t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
