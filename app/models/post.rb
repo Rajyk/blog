1e423915c40ab6b737cab8337b90f6d1e8a63935
Raj Yelamanchili <rajy@squareup.com>
2018-02-28 13:05:42 -0800

    updated alerts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,9 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, presence: true
+
+  validates_length_of :title, minimum: 5
+  validates_length_of :content, minimum: 20
+
+
 end
